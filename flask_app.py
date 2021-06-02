974f1e6d1cf0269a4f04235acb50b9a3258e0ce7
pberti <panczi_berti@yahoo.com>
2021-06-03 00:43:14 +0100

    qa

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-#photos = UploadSet('photos', IMAGES)
+import urllib.request # shop
+import os # shop
+from werkzeug.utils import secure_filename # shop
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+# shop
+UPLOAD_FOLDER = 'static/poze/'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# end shop
+
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="Albert73",
@@ -43,3 +60,34 @@ def wibble():
 @app.route("/shop", methods=["GET", "POST"])
 def shop():
     return render_template("shop.html")
+
+
+# shop
+
+@app.route('/upload')
+def home():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
