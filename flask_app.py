126dd47d5f6647ed512f2ed1d4ea72a3e77abd82
pberti <panczi_berti@yahoo.com>
2021-06-03 00:20:54 +0100

    dcsd

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-photos = UploadSet('photos', IMAGES)
+#photos = UploadSet('photos', IMAGES)
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
