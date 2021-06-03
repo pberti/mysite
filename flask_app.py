a78e7c76380dddb589168359e118f34f874b2183
pberti <panczi_berti@yahoo.com>
2021-06-03 03:41:21 +0100

    scd

--- flask_app.py
+++ flask_app.py
@@ -64,7 +64,7 @@ def shop():
 
 # shop
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def home():
     return render_template('upload.html')
 
@@ -82,14 +82,13 @@ def upload_image():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         #print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)'''
+        return render_template('index.html', filename=filename)
     else:
         flash('Allowed image types are - png, jpg, jpeg, gif')
         return redirect(request.url)
-
+'''
 @app.route('/display/<filename>')
 def display_image(filename):
     #print('display_image filename: ' + filename)
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
 '''
