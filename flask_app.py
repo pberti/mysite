512b939bf40b2f40814fc79674a30b634f20f193
pberti <panczi_berti@yahoo.com>
2021-06-03 03:34:51 +0100

    dvsd

--- flask_app.py
+++ flask_app.py
@@ -79,10 +79,10 @@ def upload_image():
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))'''
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         #print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)
+        return render_template('index.html', filename=filename)'''
     else:
         flash('Allowed image types are - png, jpg, jpeg, gif')
         return redirect(request.url)
