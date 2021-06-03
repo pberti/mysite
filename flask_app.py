8fb98c6e9e2a4750e3a5f4cf4ecb32bb5f43c2c3
pberti <panczi_berti@yahoo.com>
2021-06-03 04:25:56 +0100

    sdc

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
 from wtforms import FileField
+from flask_uploads import configure_uploads, IMAGES, UploadSet
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -47,11 +48,20 @@ def shop():
 
 # UPLOAD
 
+app.config['SECRET_KEY'] = 'thisisasecret'
+app.config['UPLOADED_IMAGES_DEST'] = 'static/poze'
+
+
+images = UploadSet('images', IMAGES)
+configure_uploads(app, images)
+
 class MyForm(FlaskForm):
     image = FileField('image')
 
-@app.route('/upload')
-def upload():
+@app.route('/upload', methods=['GET', 'POST'])
+def index():
     form = MyForm()
-
+    if form.validate_on_submit():
+        filename = images.save(form.image.data)
+        return f'Filename: { filename }'
     return render_template('upload.html', form=form)
