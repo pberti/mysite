efbcc8b794a7c12ad2e1946cf0c724a0e7566020
pberti <panczi_berti@yahoo.com>
2021-06-03 04:19:17 +0100

    evr

--- flask_app.py
+++ flask_app.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-import urllib.request # shop
-import os # shop
-from werkzeug.utils import secure_filename # shop
-
+from flask_wtf import Flaskform
+from wtforms import FileField
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
-# shop
-UPLOAD_FOLDER = 'static/poze/'
-
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-# end shop
-
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="Albert73",
@@ -62,33 +45,13 @@ def shop():
     return render_template("shop.html")
 
 
-# shop
+# UPLOAD
 
-@app.route('/upload', methods=['GET', 'POST'])
-def home():
-    return render_template('upload.html')
+class MyForm(FlaskForm):
+    image = FileField('image')
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
-'''
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
-'''
+@app.route('/upload')
+def upload():
+    form = MyForm()
+
+    return render_template('upload.html', form=form)
