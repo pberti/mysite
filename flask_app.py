0e64ea82cfe88039f8489e017002e13cc4bcf798
pberti <panczi_berti@yahoo.com>
2021-06-05 04:54:48 +0000

    salvat poza

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -43,54 +43,28 @@ def wibble():
 def shop():
     return render_template("shop.html")
 
-'''
-# UPLOAD
-
-from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
-import os
-from werkzeug.utils import secure_filename
-
-app = Flask(__name__)
-
-UPLOAD_FOLDER = 'static/uploads/'
-
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+app.config["IMAGE_UPLOADS"] = "/home/Albert73/mysite/static/poze"
+
+@app.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        return render_template('index.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
 
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            image.save(os.path.join(app.root_path, app.config["IMAGE_UPLOADS"], image.filename))
+
+            return redirect(request.url)
+    return render_template("upload_image.html")
+'''
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    return render_template("upload_image.html")
 
 '''
